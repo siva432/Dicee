5a34331c39573e19226dd93ba60bca6f43868406
Sivakumar <57859575+siva432@users.noreply.github.com>
2020-12-06 20:20:02 +0400

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "images/dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
 
